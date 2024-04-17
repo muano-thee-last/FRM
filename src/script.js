0d7613d3ebc6b637ddb211e6c3eedd9bd8b0978e
muano-thee-last <2573565@students.wits.ac.za>
2024-04-17 12:25:59 +0200

    tryna fix google log in

--- src/script.js
+++ src/script.js
@@ -8,3 +8,22 @@ function openAdminPopup() {
     document.getElementById("adminPopup").style.display = "none";
   }
   
+
+function logIn(){
+    fetch('http://localhost:5000/auth/google')
+        .then(response => {
+            // Check if response status is a success (2xx)
+            if (response.ok) {
+                // Redirect to the returned URL
+                window.location.href = response.url;
+            } else {
+                // Handle error, if needed
+                console.error('Failed to fetch Google authentication endpoint:', response.statusText);
+            }
+        })
+        .catch(error => {
+            // Handle fetch error, if any
+            console.error('Error fetching Google authentication endpoint:', error);
+        });
+}
+
